c7a35ff865ed9b1fd8def3da2c6963507713799e
Nepolean <nepolean_kh23@mtu.ac.in>
2024-12-11 05:33:35 -0800

    This is 1st commit

---
+++ Calculator-program.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    // Declare variables
+    double num1, num2, result;
+    char operator;
+
+    // Ask user for input
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);  // Note the space before %c to catch any leftover newline
+    
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    // Perform calculation based on operator
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            // Check for division by zero
+            if(num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+            break;
+    }
+
+    return 0;
+}
+
